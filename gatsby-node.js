bc6bd5a4dcb26bb749585ab541c0fb27d9d06196
Patrycja Batko <patrycja.batko@netguru.co>
2019-12-23 17:42:42 +0100

    Generate pages based on link

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,21 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
-// You can delete this file if you're not using it
+exports.createPages = async function({ actions, graphql }) {
+  const { data } = await graphql(`
+    query {
+      allDirectory(filter: { name: { ne: "images" } }) {
+        nodes {
+          absolutePath
+          name
+        }
+      }
+    }
+  `)
+  data.allDirectory.nodes.forEach(node => {
+    console.log(node)
+    const slug = node.name
+    actions.createPage({
+      path: slug,
+      component: require.resolve(`./src/components/gallery.js`),
+      context: { slug: slug },
+    })
+  })
+}
---
+++ src/components/gallery.js
@@ -0,0 +1,6 @@
+import PropTypes from "prop-types"
+import React from "react"
+
+const Gallery = ({ siteTitle }) => <div>gallery</div>
+
+export default Gallery
