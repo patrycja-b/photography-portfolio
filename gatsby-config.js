7d02adb217ff0cbd7679cab86c4c7dcc0ad3bf6a
Patrycja Batko <patrycja.batko@netguru.co>
2019-12-23 21:04:26 +0100

    Add scss

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,32 @@ module.exports = {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
+    menuLinks: [
+      {
+        name: "people",
+        link: "/people",
+        sublinks: [
+          {
+            name: "baba",
+            link: "people/baba",
+          },
+          {
+            name: "baba",
+            link: "people/second",
+          },
+        ],
+      },
+      {
+        name: "travels",
+        link: "/travels",
+        sublinks: [
+          {
+            name: "mountains",
+            link: "travels/mountains",
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -27,6 +53,7 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-sass`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
